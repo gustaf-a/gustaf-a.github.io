8ac1e921906007dbc7a1361ae34114d986f74254
Gustaf Ahlner <gustaf.ahlner@sinch.com>
2020-04-03 21:45:55 +0200

    Add external .js-file

---
+++ OutreachCheatSheet/content.js
@@ -0,0 +1,7 @@
+var classic = ["Question 1", "Q2", "Question 3", "Question 4", "Question 5"] 
+
+function getQuestionResponse(question, questionNumber){  
+    if(question==="classic"){
+        alert(classic[questionNumber]);      
+    }
+   }
